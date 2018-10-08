417fc9856daa51338a9d9cd5cea61f9851e9c512
asnatuhfah <asna.tuhfah@gmail.com>
2018-10-08 14:43:59 +0800

    perubahan di  latihan1_py

--- latihan_1.py
+++ latihan_1.py
@@ -1,1 +1,26 @@
-print 'SESUATU'
+a = 4
+A = 5
+
+kata = "python"
+kalimat = 'ini adalah string'
+paragraf ='''ini adalah string
+ini adalah string
+ini adalah string
+ini adalah string'''
+
+var_1 = True
+var_2 = "7"
+
+# print(type (int (var_1)))
+
+var_tuple = ("laki", "perempuan")
+var_list = ['senin','selasa','rabu','kamis',"jum'at",'sabtu',1,2]
+dict_contoh= {
+    'nama':'Asna',
+    'alamat':'Kekalik',
+    'kelamin':'Perempuan'
+}
+
+list_ = [1,5,7]
+list_.append(dict_contoh)
+for i in range(0,len(list_)):print(list_[i])
