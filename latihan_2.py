d0b4bf797f2257e18a332dc9ea41c50c541213d7
asnatuhfah <asna.tuhfah@gmail.com>
2018-10-09 09:48:46 +0800

    modified

---
+++ latihan_2.py
@@ -0,0 +1,5 @@
+# type conversi
+angka = 4
+string ="4"
+var_1 = angka + int(string)
+print (var_1)
---
+++ latihan_3.py
@@ -0,0 +1,6 @@
+# Aritmatika
+x = 6
+y = 2
+
+print("hasil bagi x*y = %s" %(x*y))
+print("hasil kali x/y = %s" %(x/y))
